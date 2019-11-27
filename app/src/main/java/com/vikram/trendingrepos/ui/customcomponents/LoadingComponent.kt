c3100d3681294285aa3aa2c9594a1541b67b71a3
vikramsharma <vikramsharma654@gmail.com>
2019-11-27 20:30:37 +0530

    feat: added loading component and shimmer library

---
+++ app/src/main/java/com/vikram/trendingrepos/ui/customcomponents/LoadingComponent.kt
@@ -0,0 +1,20 @@
+package com.vikram.trendingrepos.ui.customcomponents
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import com.vikram.trendingrepos.databinding.ComponentLoadingBinding
+
+class LoadingComponent(
+    context: Context?,
+    attrs: AttributeSet?
+) : LinearLayout(context, attrs) {
+
+    init {
+       val binding = ComponentLoadingBinding.inflate(LayoutInflater.from(context))
+        addView(binding.root)
+        binding.shimmerViewContainer.startShimmer()
+    }
+
+}
