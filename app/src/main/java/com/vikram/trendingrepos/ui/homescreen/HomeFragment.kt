40bdb1a8b7a809a10ed6338c45a33c6ea81d93d9
vikramsharma <vikramsharma654@gmail.com>
2019-11-28 04:29:03 +0530

    feat: Repository rows are shown into recycler view

--- app/src/main/java/com/vikram/trendingrepos/ui/homescreen/HomeFragment.kt
+++ app/src/main/java/com/vikram/trendingrepos/ui/homescreen/HomeFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.vikram.trendingrepos.R
 import com.vikram.trendingrepos.TrendingReposApp
+import com.vikram.trendingrepos.data.model.NetworkResponse
 import com.vikram.trendingrepos.databinding.FragmentHomeBinding
 import com.vikram.trendingrepos.ui.base.BaseFragment
+import com.vikram.trendingrepos.utils.hide
+import com.vikram.trendingrepos.utils.show
 
 class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
 
@@ -26,7 +32,60 @@ class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initUI()
+        viewModel.getRepositories()
+    }
 
+    private fun initUI() {
+        binding.rvRepository.layoutManager =
+            LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        initObservers()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.errorLayout.btnRetry.setOnClickListener {
+            viewModel.getRepositories()
+        }
+        binding.swipeRefresh.setOnRefreshListener {
+            viewModel.forceRefreshRepositories()
+            binding.swipeRefresh.isRefreshing = false
+        }
+    }
+
+    private fun initObservers() {
+        viewModel.getResponseLiveData().observe(this, Observer {
+            when (it) {
+                is NetworkResponse.Loading -> {
+                    showLoading()
+                }
+                is NetworkResponse.Success -> {
+                    loadList(it.data)
+                }
+                is NetworkResponse.ResponseError -> {
+                    showError()
+                }
+            }
+        })
+    }
+
+    private fun showError() {
+        binding.errorLayout.root.show()
+        binding.viewLoading.hide()
+        binding.rvRepository.hide()
+    }
+
+    private fun showLoading() {
+        binding.viewLoading.show()
+        binding.rvRepository.hide()
+        binding.errorLayout.root.hide()
+    }
+
+    fun loadList(list: List<RepositoryItem>) {
+        binding.viewLoading.hide()
+        binding.rvRepository.show()
+        binding.errorLayout.root.hide()
+        binding.rvRepository.adapter = RepositoryAdapter(list)
     }
 
 
--- app/src/main/java/com/vikram/trendingrepos/ui/homescreen/HomeViewModel.kt
+++ app/src/main/java/com/vikram/trendingrepos/ui/homescreen/HomeViewModel.kt
@@ -1,8 +1,53 @@
 package com.vikram.trendingrepos.ui.homescreen
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.vikram.trendingrepos.data.model.NetworkResponse
+import com.vikram.trendingrepos.data.repository.Repository
 import com.vikram.trendingrepos.ui.base.BaseViewModel
+import com.vikram.trendingrepos.utils.IRxSchedulers
+import com.vikram.trendingrepos.utils.toRepositoryItemList
 import javax.inject.Inject
 
-class HomeViewModel @Inject  constructor(): BaseViewModel() {
+class HomeViewModel @Inject constructor(
+    private val repository: Repository,
+    private val iRxSchedulers: IRxSchedulers
+) : BaseViewModel() {
+
+    private val responseLiveData: MutableLiveData<NetworkResponse<List<RepositoryItem>>> =
+        MutableLiveData()
+
+    fun getResponseLiveData(): LiveData<NetworkResponse<List<RepositoryItem>>> = responseLiveData
+
+    fun getRepositories() {
+        val disposable = repository.getTrendingRepositories().subscribeOn(iRxSchedulers.io())
+            .observeOn(iRxSchedulers.main()).doOnSubscribe {
+                responseLiveData.value = NetworkResponse.Loading
+            }.subscribe({ repositories ->
+                responseLiveData.value =
+                    NetworkResponse.Success(repositories.toRepositoryItemList())
+            }, { error ->
+                responseLiveData.value = NetworkResponse.ResponseError(error)
+            })
+        addDisposable(disposable)
+    }
+
+    /**
+     * this method will ignore cache and fetch new results from server
+     *
+     */
+    fun forceRefreshRepositories() {
+        val disposable = repository.getTrendingRepositories().subscribeOn(iRxSchedulers.io())
+            .observeOn(iRxSchedulers.main()).doOnSubscribe {
+                responseLiveData.value = NetworkResponse.Loading
+            }.subscribe({ repositories ->
+                responseLiveData.value =
+                    NetworkResponse.Success(repositories.toRepositoryItemList())
+            }, { error ->
+                responseLiveData.value = NetworkResponse.ResponseError(error)
+            })
+        addDisposable(disposable)
+    }
+
 
 }
